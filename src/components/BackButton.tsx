5d42560a8b5edae9b733c6fe39233bda5a096c00
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-09-20 23:48:08 +0100

    fix: reduce font size of back button text

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,14 +1,14 @@
 import { Feather } from '@expo/vector-icons';
 import { Button, Text, useTheme } from '@/theme';
 
-export const BackButton = ({  text='Back' }: {  text?:string }) => {
+export const BackButton = ({ text = 'Back' }: { text?: string }) => {
   const { colors } = useTheme();
 
   return (
-    <Button flexDirection="row" paddingLeft="m_16" onPress={() => void 0}>
-      <Feather name="chevron-left" size={16} color={colors.text} />
-      <Text marginLeft="xs_4" variant='large' color="text" >
-       {text}
+    <Button variant="link" flexDirection="row" paddingLeft="m_16" onPress={() => void 0}>
+      <Feather name="chevron-left" size={20} color={colors.text} />
+      <Text marginLeft="xs_4" variant="title" color="text">
+        {text}
       </Text>
     </Button>
   );
