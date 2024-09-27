7659205763c506443fec9b703a1170817ee9ae8c
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-09-27 15:57:25 +0100

    refactor(app/screens): update VerifyScreen component to handle email code verification for sign in and sign up

--- app/src/screens/Auth/VerifyScreen.tsx
+++ app/src/screens/Auth/VerifyScreen.tsx
@@ -1,10 +1,11 @@
 import { Container } from '@/components/Container';
 import { AuthStackParamList } from '@/navigation/AuthStack';
 import { Routes } from '@/navigation/routes';
-import { Box, Button, makeStyles, Text } from '@/theme';
+import { Box, makeStyles, Text } from '@/theme';
+import { isClerkAPIResponseError, useSignIn, useSignUp } from '@clerk/clerk-expo';
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { Keyboard, Platform, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Alert, Keyboard, Platform, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import {
   CodeField,
   Cursor,
@@ -13,9 +14,15 @@ import {
 } from 'react-native-confirmation-code-field';
 const CELL_COUNT = 6;
 export default function VerifyScreen({
-  route,
+  route: {
+    params: { email, isSignIn },
+  },
 }: StackScreenProps<AuthStackParamList, Routes.VERIFY>) {
+  const { signUp, isLoaded: isSignUpLoaded, setActive } = useSignUp();
+  const { signIn, isLoaded: isSignInLoaded } = useSignIn();
   const [code, setCode] = React.useState('');
+  const [isError, setIsError] = React.useState(false);
+  const [isVerifying, setIsVerifying] = React.useState(false);
   const ref = useBlurOnFulfill({ value: '', cellCount: CELL_COUNT });
   const [props, getCellOnLayoutHandler] = useClearByFocusCell({
     value: '',
@@ -23,6 +30,65 @@ export default function VerifyScreen({
   });
   const themeStyles = React.useMemo(() => styles(), []);
   const autoComplete = Platform.OS === 'android' ? 'sms-otp' : 'one-time-code';
+  const verifySignIn = async () => {
+    if (!isSignInLoaded && !signIn) return null;
+    if (typeof setActive !== 'function') return null;
+    setIsVerifying(true);
+    try {
+      const signInAttempt = await signIn.attemptFirstFactor({
+        code,
+        strategy: 'email_code',
+      });
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId });
+      } else {
+        console.error(signInAttempt);
+      }
+    } catch (err) {
+      console.error('Error:', JSON.stringify(err, null, 2));
+      if (isClerkAPIResponseError(err)) {
+        setIsError(true);
+      }
+      Alert.alert('Error', 'Failed to Verify Email');
+    } finally {
+      setIsVerifying(false);
+    }
+  };
+  const verifySignUp = async () => {
+    if (!isSignUpLoaded && !signUp) return null;
+    if (typeof setActive !== 'function') return null;
+    setIsVerifying(true);
+    try {
+      const signInAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId });
+      } else {
+        console.error(signInAttempt);
+      }
+    } catch (err) {
+      console.error('Error:', JSON.stringify(err, null, 2));
+      if (isClerkAPIResponseError(err)) {
+        setIsError(true);
+      }
+      Alert.alert('Error', 'Failed to Verify Email');
+    } finally {
+      setIsVerifying(false);
+    }
+  };
+  React.useEffect(() => {
+    if (code.length === 6 && !isVerifying) {
+      if (isError) {
+        setIsError(false);
+      }
+      if (isSignIn) {
+        verifySignIn();
+      } else {
+        verifySignUp();
+      }
+    }
+  }, [code]);
   return (
     <Container>
       <Box width="100%" flex={1} padding="m_24">
@@ -35,11 +101,10 @@ export default function VerifyScreen({
               <Text variant="body" color="neutral" lineHeight={25}>
                 Please fill the input box with the six digit code sent to{' '}
                 <Text color="text" fontWeight={'semibold'}>
-                  {route.params.email}
+                  {email}
                 </Text>{' '}
               </Text>
             </Box>
-
             <CodeField
               ref={ref}
               {...props}
@@ -53,7 +118,13 @@ export default function VerifyScreen({
               renderCell={({ index, symbol, isFocused }) => (
                 <React.Fragment key={index}>
                   <Text
-                    style={[themeStyles.cell, isFocused && themeStyles.focusCell]}
+                    disabled={isVerifying}
+                    style={[
+                      themeStyles.cell,
+                      isFocused && themeStyles.focusCell,
+                      isError && themeStyles.errorCell,
+                      isVerifying && themeStyles.disabledCell,
+                    ]}
                     onLayout={getCellOnLayoutHandler(index)}>
                     {symbol || (isFocused ? <Cursor /> : null)}
                   </Text>
@@ -62,8 +133,6 @@ export default function VerifyScreen({
                 </React.Fragment>
               )}
             />
-            <Box style={{ marginTop: 10 }}></Box>
-            <Button label="Verify" />
           </>
         </TouchableWithoutFeedback>
       </Box>
@@ -86,8 +155,15 @@ const styles = makeStyles((theme) =>
       textAlign: 'center',
       borderRadius: theme.borderRadii.s_4,
     },
+    disabledCell: {
+      backgroundColor: theme.colors.inactive,
+      opacity: 0.5,
+    },
     focusCell: {
       borderColor: theme.colors.muted,
+    },
+    errorCell: {
+      borderColor: theme.colors.destructive,
     },
     cellSeperator: {
       width: 26,
